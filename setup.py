cb01b78b46b7a9cf5c513a8e31ea9fe6ccee9462
Sri Panyam <sri.panyam@gmail.com>
2022-03-13 01:29:23 -0800

    Updated protos and setup configs

--- setup.py
+++ setup.py
@@ -1,25 +1,6 @@
-import setuptools
+from setuptools import setup
 
-with open("README.md", "r", encoding="utf-8") as fh:
-    long_description = fh.read()
-
-setuptools.setup(
-    name="pypslite",
-    version="0.0.1",
-    author="Sri Panyam",
-    description="Python GRPC Bindings for pslite - A simple local pubsub queue",
-    long_description=long_description,
-    long_description_content_type="text/markdown",
-    url="https://github.com/panyam/pypslite",
-    project_urls={
-        "Bug Tracker": "https://github.com/panyam/pypslite/issues",
-    },
-    classifiers=[
-        "Programming Language :: Python :: 3",
-        "License :: OSI Approved :: MIT License",
-        "Operating System :: OS Independent",
-    ],
-    package_dir={"": "."},
-    packages=setuptools.find_packages(where="src"),
-    python_requires=">=3.6",
+# Metadata goes in setup.cfg. These are here for GitHub's dependency graph.
+setup(
+    name="PyPSLite",
 )
